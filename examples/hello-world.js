5a1e601f1911518c120fcb1a6764c0c749bf9a7a
Katie Tensfeldt <kathleenmmoore@live.com>
2020-05-14 13:22:35 -0700

    Add notes section

--- examples/hello-world.js
+++ examples/hello-world.js
@@ -1,12 +1,10 @@
 <html>
   <head>
-  
     <title>Hello, World!</title>
-    
   </head>
   
   <body>
-  
+  //Here is where the JavaScript will be embeded
     <script>
       alert("Hello, World!");
     </script>
